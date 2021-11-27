43672bfe62d1f08e94cd8715d8776952ed2d4501
HYW <790949607@qq.com>
2021-11-27 13:54:24 +0800

    hot-fix commit

--- src/main/java/com/gx/Hello.java
+++ src/main/java/com/gx/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git 1");
         System.out.println("hello git 1");
+        System.out.println("hello git 2");
     }
 
 }
